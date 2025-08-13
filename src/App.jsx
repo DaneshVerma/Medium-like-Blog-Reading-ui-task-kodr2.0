236b884a36c51bcc1635b2ee3187561d0c9668a3
Danesh <techydanesh@gmail.com>
2025-08-13 21:23:53 +0530

    data entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Post from './pages/Post';
+import ScrollToTop from './components/ScrollTop';
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<Post />} />
--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -12,7 +12,7 @@ export default function BlogCard({ post }) {
         alt={post.title}
         className='w-full h-48 object-cover group-hover:scale-105 transition'
       />
-      <div className='p-5 flex flex-col gap-3 flex-1'>
+      <div className='p-5 flex justify-between flex-col gap-3 flex-1'>
         <h2 className='text-2xl font-bold text-gray-900 dark:text-white mb-2 group-hover:text-blue-600 transition'>
           {post.title}
         </h2>
@@ -20,11 +20,13 @@ export default function BlogCard({ post }) {
           {post.excerpt}
         </p>
         <div className='flex items-center mt-3 gap-2'>
-          <img
-            src={post.avatar}
-            alt={post.author}
-            className='w-8 h-8 rounded-full'
-          />
+          <div className='w-10 h-10  rounded-full overflow-hidden'>
+            <img
+              src={post.avatar}
+              alt={post.author}
+              className='object-cover w-full h-full rounded-full'
+            />
+          </div>  
           <span className='text-sm text-gray-600 dark:text-gray-400'>
             {post.author}
           </span>
